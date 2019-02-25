80f36dd1842a1863c54ba7fa9fdef97fcc4c7483
Entropy Xu <entropy.xcy@protonmail.com>
2019-02-25 01:03:49 -0800

    Basic structure finished

--- README.md
+++ README.md
@@ -9,13 +9,20 @@ A helpful little tool for generating the config file for frp.
 * -l or --local_ip : Set the local ip address
 * -s or --server_ip : Set the server ip address and port
 * -p or --port : Set port to expose
+* -ap or --additional_port : Set expose for ports on LAN ip other than which -l specifies. Notice that for this option 
+you have to specifies a port on server side explicitly. Check for Example.5.
 * -o or --output ： Set the name of the output file; By default, the output file is "frpc.ini"
 * -h or --help ： Show this help page
+* -v or --verbose ： Verbose mode
 
 ## Examples
-* Expose port 22 and port 80 to public IP 8.8.8.8 from LAN ip 0.0.0.0. Notice that the port on the server running frp service is 32767 <br/>
+1. Expose port 22 and port 80 to public IP 8.8.8.8 from LAN ip 0.0.0.0. Notice that the port on the server running frp service is 32767 <br/>
 ```python3 frp_generator.py -l 0.0.0.0 -p 22 -p 80 -s 8.8.8.8:32767```
-* Append one more port 443 to the existing config file "frpc.ini" <br/>
+2. Append one more port 443 to the existing config file "frpc.ini" <br/>
 ```python3 frp_generator.py -a -p 443```
-* Append port range from 6000 to 6010 to the existing config file "config.ini" <br/>
+3. Append port range from 6000 to 6010 to the existing config file "config.ini" <br/>
 ```python3 frp_generator.py -a config.ini -p 6000-6010```
+4. Expose port 22 from LAN ip 0.0.0.0 to port 222 of public IP 8.8.8.8. Notice that the port on the server running frp service is 32767 <br/>
+```python3 frp_generator.py -l 0.0.0.0 -p 22->222 -s 8.8.8.8:32767```
+5. Expose port 22 from LAN ip 192.168.1.1 to port 22 and expose LAN ip 192.168.1.2 to port 222 on server 8.8.8.8:32767 <br/>
+```python3 frp_generator.py -p 192.168.1.1:22->22 -p 192.168.1.2:22->222```
--- frp_generator.py
+++ frp_generator.py
@@ -1,10 +1,90 @@
 import sys
 
 
-def argv_handler(argv):
-    print(argv)
+def gui_handler():
+    print("We are currently not supporting GUI yet!")
+    exit()
 
 
 if __name__ == "__main__":
     argv = sys.argv
-    argv_handler(argv)
+
+    # mode = 0 for create mode
+    # mode = 1 for append mode
+    mode = 0
+
+    # gui = 0 for not using gui
+    # gui = 1 for using gui
+    gui = 0
+
+    # verbose = 0 for not using verbose mode
+    # verbose = 1 for using verbose mode
+    verbose = 0
+
+    local_ip = ""
+
+    server_ip = ""
+    # server_port = -1 for a invalid port
+    server_port = -1
+
+    port_list = {}
+
+    filename = ""
+
+    for i in range(len(argv)):
+        # append
+        if argv[i] == "-a" or argv[i] == "--append":
+            mode = 1
+            if argv[i + 1].find("-") == -1:
+                # The user specifies a filename
+                filename = argv[i + 1]
+
+        # gui
+        if argv[i] == "-g" or argv[i] == "--gui":
+            gui = 1
+            gui_handler()
+
+        # local ip
+        if argv[i] == "-l" or argv[i] == "--local_ip":
+            local_ip = argv[i + 1]
+
+        # server ip
+        if argv[i] == "-s" or argv[i] == "--server_ip":
+            if argv[i + 1].find(":") != -1:
+                # The address does contains a port
+                ip_port = argv[i + 1].split(":")
+                print(ip_port)
+                server_ip = ip_port[0]
+                server_port = ip_port[1]
+            else:
+                print("Error: -s parameter should followed by both ip address and port")
+                print("For example: -s 8.8.8.8:80")
+                print("Invalid Input, Aborting!")
+                exit()
+
+        # port
+        if argv[i] == "-p" or argv[i + 1] == "--port":
+            if argv[i + 1].find("->") != -1:
+                # The user specifies a port translation
+                pass
+            elif argv[i + 1].find("-") != -1:
+                # The user specifies a port range
+                pass
+            else:
+                # The user specifies a normal port
+                pass
+
+        # verbose
+        if argv[i] == "-v" or argv[i] == "-verbose":
+            verbose = 1
+
+    if verbose:
+        print("Mode:", mode)
+        print("GUI:", gui)
+        print("Verbose:", verbose)
+        print("Local IP:", local_ip)
+        print("Server IP:", server_ip)
+        print("Server Port", server_port)
+        print("Port List:", port_list)
+        print("Filename:", filename)
+
